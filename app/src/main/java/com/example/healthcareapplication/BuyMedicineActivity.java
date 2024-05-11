7ccfb92108b48c04fecd2b2b48b4d989447507fc
Manar <manarbouoni@gmail.com>
2024-05-11 23:34:54 +0100

    commit

---
+++ app/src/main/java/com/example/healthcareapplication/BuyMedicineActivity.java
@@ -0,0 +1,99 @@
+package com.example.healthcareapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BuyMedicineActivity extends AppCompatActivity {
+
+    private String[][] packages =
+            {
+                    {"Uprise-D3 1000IU Capsule", "", "", "", "50"},
+                    {"HealthVit Chromium", "", "", "", "305"},
+                    {"Vitamin A", "", "", "", "447"},
+                    {"1 mg Calicium", "", "", "", "539"},
+                    {"Inlife Vitamine E", "", "", "", "30"},
+                    {"Vitamin C", "", "", "", "40"},
+
+
+            };
+
+    private String[] package_details = {
+            "Detailed description of Uprise-D3 1000IU Capsule\n",
+            "Detailed description of HealthVit Chromium\n",
+            "Detailed description of Vitamin A\n",
+            "Detailed description of 1 mg Calcium\n",
+            "Detailed description of Inlife Vitamin E\n",
+            "Detailed description of Vitamin C"
+
+    };
+    HashMap<String, String> item;
+    ArrayList list;
+    SimpleAdapter sa;
+    ListView lst;
+    Button btnBack, btnGoToCart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_medicine);
+
+        lst = findViewById(R.id.listViewArticle);
+        btnBack = findViewById(R.id.buttonBMCCheckout);
+        btnGoToCart = findViewById(R.id.buttonBMGoToCart);
+
+        btnGoToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BuyMedicineActivity.this,CartBuyMedicineActivity.class));
+
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BuyMedicineActivity.this, HomeActivity.class));
+            }
+        });
+
+        list = new ArrayList();
+        for(int i=0; i<packages.length; i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", packages[i][0]);
+            item.put("line2", packages[i][1]);
+            item.put("line3", packages[i][2]);
+            item.put("line4", packages[i][3]);
+            item.put("line5", "Total Cost:" + packages[i][4] + "/-");
+            list.add(item);
+
+        }
+
+
+        sa = new SimpleAdapter(this, list,
+                R.layout.multi_lines,
+                new String[] {"line1", "line2", "line3", "line4", "line5"},
+                new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});
+        lst.setAdapter(sa);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(BuyMedicineActivity.this, BuyMedicineDetailsActivity.class);
+                it.putExtra("text1", packages[i][0]);
+                it.putExtra("text2", package_details[i]);
+                it.putExtra("text3", packages[i][4]);
+                startActivity(it);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/healthcareapplication/FindDoctorActivity.java
@@ -0,0 +1,86 @@
+package com.example.healthcareapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FindDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_doctor);
+
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class));
+
+
+            }
+        });
+
+        CardView familyphysician = findViewById(R.id.cardFDFamilyPhysician);
+        familyphysician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Family Physicians");
+
+
+                startActivity(it);
+            }
+        });
+
+        CardView dietician = findViewById(R.id.cardFDDieticien);
+        dietician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Dietician");
+                startActivity(it);
+            }
+        });
+
+        CardView dentist = findViewById(R.id.cardFDDentist);
+        dentist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Dentist");
+                startActivity(it);
+            }
+        });
+
+        CardView surgeon = findViewById(R.id.cardFDSurgeaon);
+
+        surgeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "Surgeon");
+                startActivity(it);
+            }
+
+        });
+
+
+        CardView cardioLogists = findViewById(R.id.cardFDCardiologists);
+
+        cardioLogists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FindDoctorActivity.this, DoctorDetailsActivity.class);
+                it.putExtra("title", "CardioLogists");
+                startActivity(it);
+            }
+
+        });
+
+
+    }
+}
